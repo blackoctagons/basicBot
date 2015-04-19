d5b360f79f13bc803b632a867a5865b69afd7bb1
blackoctagons <hnic@myninjaplease.com>
2015-04-19 17:17:12 -0400

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1616,10 +1616,7 @@
                      	    var bonuses;
                      	    var randBon;
                      	    var bonusarray;
-                     	    
-                     	    $.get('bonus.txt',function(data){
-			  var bonuses=data.split('\n');
-					    };
+                     	
                     	randBon = Math.floor(Math.random() * bonuses.length);
                             API.sendChat(bonuses[randBon] + " SOOWOO!!!");
                             
